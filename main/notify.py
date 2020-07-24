10d0e19a54403e6b6a239aba9c7241c6b611729c
Cris Idemoto <crisidemoto@gmail.com>
2020-07-24 13:49:47 -0700

    addressed pylint warnings for `notify.py`

--- main/notify.py
+++ main/notify.py
@@ -1,40 +1,34 @@
-""" TODO: add documentation for this """
-from django.conf import settings
-import slack
-
 """ Class to handle admin notification in tator online
 
 TODO: Handle other forms of notify in here too (e.g. SMTP)
 """
+from django.conf import settings
+import slack
 
 class Notify:
     """ TODO: add documentation for this """
+    @staticmethod
     def notification_enabled():
         """ Returns true if notification is enabled """
         return settings.TATOR_SLACK_TOKEN and settings.TATOR_SLACK_CHANNEL
 
-    def notify_admin_msg(self, msg):
+    @staticmethod
+    def notify_admin_msg(msg):
         """ Sends a given message to administrators """
         if Notify.notification_enabled():
             client = slack.WebClient(token=settings.TATOR_SLACK_TOKEN)
             response = client.chat_postMessage(channel=settings.TATOR_SLACK_CHANNEL,
                                                text=msg)
-            if response['ok']:
-                return True
-            else:
-                return False
-        else:
-            return False
-    def notify_admin_file(self, title, content):
+            return bool(response['ok'])
+        return False
+
+    @staticmethod
+    def notify_admin_file(title, content):
         """ Send a given file to administrators """
         if Notify.notification_enabled():
             client = slack.WebClient(token=settings.TATOR_SLACK_TOKEN)
             response = client.files_upload(channels=settings.TATOR_SLACK_CHANNEL,
                                            content=content,
                                            title=title)
-            if response['ok']:
-                return True
-            else:
-                return False
-        else:
-            return False
+            return bool(response['ok'])
+        return False
